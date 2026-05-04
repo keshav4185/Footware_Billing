1f1e88006ae476549e1a2554d14e7ee463709844
Kaushal Mali <kaushalmali@Kaushals-Laptop-3.local>
2026-05-04 16:03:28 +0530

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://backend-billing-software-ahxt.onrender.com/api';
+const API_BASE_URL = 'http://localhost:8080/api';
 
 // Create axios instance with default config
 const api = axios.create({
